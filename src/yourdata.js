e4d0aee927e76964dcb2a104714477bffe410bf5
dannykhoai <dvu2758@gmail.com>
2021-01-28 23:22:05 +0200

    new updates

---
+++ src/yourdata.js
@@ -0,0 +1,210 @@
+// Skills Icons
+import htmlIcon from "./images/html.svg"
+import cssIcon from "./images/css.svg"
+import reactIcon from "./images/react.svg"
+import jsIcon from "./images/javascript.svg"
+import designIcon from "./images/design.svg"
+import codeIcon from "./images/code.svg"
+import ts from "./images/ts.png"
+
+
+// Social Icon
+import githubIcon from "./images/github.svg"
+import codepenIcon from "./images/codepen.svg"
+import dribbbleIcon from "./images/dribbble.svg"
+import instagramIcon from "./images/instagram.svg"
+
+export default {
+  //(Please Do Not Remove The comma(,) after every variable)
+  //Change The Website Template
+
+  //   Header Details ---------------------
+  name: "DungVu",
+  headerTagline: [
+    //Line 1 For Header
+    "Ready for",
+    //Line 2 For Header
+    " ",
+    //Line 3 For Header
+    "New Challenges",
+  ],
+  //   Header Paragraph
+  headerParagraph:
+    "I am in love with the way digital products are timeless, weightless, and are instantly available around the world.",
+
+  //Contact Email
+  contactEmail: "dvu2758@gmail.com",
+
+  // End Header Details -----------------------
+
+  // Work Section ------------------------
+  projects: [
+    {
+      id: 1, //DO NOT CHANGE THIS (Please)😅
+      title: "Project Chatrum", //Project Title - Add Your Project Title Here
+      para:
+        "Instant chat app built on React.js, fast and robust", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://github.com/dannykhoai/chatrum/raw/master/chatrum-test.gif",
+      //Project URL - Add Your Project Url Here
+      url: "https://github.com/dannykhoai/chatrum",
+    },
+    {
+      id: 2, //DO NOT CHANGE THIS (Please)😅
+      title: "Project Fotogrid", //Project Title - Add Your Project Title Here
+      para:
+        "An open photo album app built on React.js & Firebase", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://github.com/dannykhoai/fotogrid/blob/master/fotogrid1.gif?raw=true",
+      //Project URL - Add Your Project Url Here
+      url: "https://github.com/dannykhoai/fotogrid",
+    },
+    {
+      id: 3, //DO NOT CHANGE THIS (Please)😅
+      title: "Webface99", //Project Title - Add Your Project Title Here
+      para:
+        "Facial detection app with Emotion prediction.", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://dannykhoai.github.io/aboutme/src/screenshot.png",
+      //Project URL - Add Your Project Url Here
+      url: "https://dannykhoai.github.io/webface99/",
+    },
+    {
+      id: 4, //DO NOT CHANGE THIS (Please)😅
+      title: "Robot Competition", //Project Title - Add Your Project Title Here
+      para:
+        "Program path-following robot using C & MQTT", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://dannykhoai.github.io/aboutme/src/robot.gif",
+      //Project URL - Add Your Project Url Here
+      url: "https://github.com/RenMai/Team8ZumoSD2018",
+    },
+    {
+      id: 5, //DO NOT CHANGE THIS (Please)😅
+      title: "Wolt Carousel", //Project Title - Add Your Project Title Here
+      para:
+        "Lightweight & user friendly React interface ", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://github.com/dannykhoai/woltassignment2021/raw/main/demo.gif",
+      //Project URL - Add Your Project Url Here
+      url: "https://github.com/dannykhoai/woltassignment2021",
+    },
+    {
+      id: 6, //DO NOT CHANGE THIS (Please)😅
+      title: "icOnixx eyewear", //Project Title - Add Your Project Title Here
+      para:
+        "Personal Merchandise project", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://scontent-hel3-1.xx.fbcdn.net/v/t31.0-8/14242482_180340469058769_3565774429193665444_o.jpg?_nc_cat=111&ccb=2&_nc_sid=cdbe9c&_nc_ohc=-iAHTgPntHoAX9MJIE_&_nc_ht=scontent-hel3-1.xx&oh=466fc0b698280c6b3b6d5a097d8a1add&oe=6036F990",
+      //Project URL - Add Your Project Url Here
+      url: "http://fb.com/iconixx",
+    },
+
+    /*
+    If You Want To Add More Project just Copy and Paste This At The End (Update the id Respectively)
+    ,{
+        id: 5,
+        title: 'Project Five',
+        para: 'Something Amazing',
+        imageSrc: "",
+        url: ''
+    }
+    */
+  ],
+
+  // End Work Section -----------------------
+
+  // About Secton --------------
+  aboutParaOne:
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+  aboutParaTwo:
+    "It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+  aboutParaThree:
+    "It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+  aboutImage:
+    "https://i.imgur.com/U1MzoWv.jpg",
+
+  //   End About Section ---------------------
+
+  // Skills Section ---------------
+
+  //   Import Icons from the top and link it here
+
+  skills: [
+    {
+      id: 1,
+      img: htmlIcon,
+      para:
+        "HTML5: Bread for web developer",
+    },
+    {
+      id: 2,
+      img: cssIcon,
+      para:
+        "CSS: add some butter",
+    },
+    {
+      id: 3,
+      img: jsIcon,
+      para:
+        "Javascript: add some ham",
+    },
+    {
+      id: 4,
+      img: reactIcon,
+      para:
+        "React.js together with MaterialUI & Framer Motion are my favorites",
+    },
+    {
+      id: 5,
+      img: ts,
+      para:
+        "I recently picked up Typescript and instantly love it for static typing support",
+    },
+    {
+      id: 6,
+      img: codeIcon,
+      para:
+        "C, Python, and in the future more to come ",
+    },
+  ],
+
+  // End Skills Section --------------------------
+
+  //   Promotion Section --------------------------
+
+  promotionHeading: "Heading",
+  promotionPara:
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+  // End Promotion Section -----------------
+
+  //   Contact Section --------------
+
+  contactSubHeading: "Let's create your next experience together",
+  social: [
+    // Add Or Remove The Link Accordingly
+    { img: githubIcon, url: "https://github.com/chetanverma16" },
+    {
+      img: codepenIcon,
+      url: "https://www.codepen.com/",
+    },
+    {
+      img: dribbbleIcon,
+      url: "https://dribbble.com/chetanverma",
+    },
+    {
+      img: instagramIcon,
+      url: "https://www.instagram.com/maybe_vu/",
+    },
+  ],
+
+  // End Contact Section ---------------
+}
+
+// Thanks for using this template, I would love to hear from you contact me at hello@chetanverma.com
